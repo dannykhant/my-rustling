9bcc0a53c04d4edfc01231e9f42bc439ae8d2511
dannykhant <dannypmkhant@gmail.com>
2025-08-25 00:15:20 +0700

    Init project

---
+++ rust101/guessing_game/src/main.rs
@@ -0,0 +1,21 @@
+use std::io;
+
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number...");
+
+    let secret_num = rand::rng().random_range(1..=100);
+
+    println!("The secret number is: {secret_num}");
+
+    println!("Input your guess...");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+}
