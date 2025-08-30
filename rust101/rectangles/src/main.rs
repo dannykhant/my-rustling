2ea66e067c19f2cd0e1f46b998e5c83cb3c9d563
dannykhant <dannypmkhant@gmail.com>
2025-08-30 23:22:00 +0700

    Add struct methods

--- rust101/rectangles/src/main.rs
+++ rust101/rectangles/src/main.rs
@@ -4,6 +4,16 @@ struct Rectangle {
     height: u32
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     let scale = 2;
     let rect1 = Rectangle {
@@ -14,6 +24,20 @@ fn main() {
     println!("Area: {}", area(&rect1));
     dbg!(&rect1);
     println!("{rect1:?}");
+    println!("Rect1 from method: {}", rect1.area());
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45
+    };
+
+    println!("rect1 hold rect2: {}", rect1.can_hold(&rect2));
+    println!("rect1 hold rect3: {}", rect1.can_hold(&rect3));
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
