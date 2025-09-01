952ec7f62e44ce81c7925725f144c488781a9531
dannykhant <dannypmkhant@gmail.com>
2025-09-01 23:28:32 +0700

    Matching Option<T>

--- rust101/pattern_matching/src/main.rs
+++ rust101/pattern_matching/src/main.rs
@@ -26,7 +26,19 @@ fn value_in_cents(coin: Coin) -> u8 {
     }
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1)
+    }
+}
+
 fn main() {
     let quater = value_in_cents(Coin::Quarter(UsState::Alaska));
     println!("{quater}");
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("{five:?}, {six:?}, {none:?}");
 }
