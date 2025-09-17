4fb4a1ec839e5de746567ce9896f67648e49f08f
dannykhant <dannypmkhant@gmail.com>
2025-09-17 23:05:59 +0700

    [minigrep] logic extraction

--- rust101/c12_minigrep/src/main.rs
+++ rust101/c12_minigrep/src/main.rs
@@ -1,18 +1,23 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem with args: {err}");
+        process::exit(1);
+    });
 
     println!("searching for {}", config.query);
     println!("in file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-            .expect("Can't read the file..");
-
-    println!("with text:\n{contents}");
+    if let Err(e) = run(config) {
+        println!("App error: {e}");
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -21,10 +26,22 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough args..");
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("with text:\n{contents}");
+
+    Ok(())
 }
