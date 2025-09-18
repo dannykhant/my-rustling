70f2046e19efc32267ebc8a46de5f86353fc967b
dannykhant <dannypmkhant@gmail.com>
2025-09-18 23:10:11 +0700

    [minigrep] test-driven implementation

--- rust101/c12_minigrep/src/lib.rs
+++ rust101/c12_minigrep/src/lib.rs
@@ -1,3 +1,26 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+Safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["Safe, fast, productive."], search(query, contents));
+    }
 }
