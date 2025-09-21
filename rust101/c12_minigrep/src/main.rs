3e5d99c9fd0d740d5b45bf0e09d02284b152c199
dannykhant <dannypmkhant@gmail.com>
2025-09-21 22:30:19 +0700

    [minigrep] env var

--- rust101/c12_minigrep/src/main.rs
+++ rust101/c12_minigrep/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 use std::process;
 use std::error::Error;
 
-use c12_minigrep::search;
+use c12_minigrep::{search, search_case_insensitive};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -23,8 +23,9 @@ fn main() {
 }
 
 struct Config {
-    query: String,
-    file_path: String
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool
 }
 
 impl Config {
@@ -36,14 +37,22 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}")
     }
 
