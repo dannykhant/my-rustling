c291de57d213f3932fb31bd52590e2375dd83e54
dannykhant <dannypmkhant@gmail.com>
2025-10-15 22:49:40 +0700

    Add Rc smart pointer

---
+++ rust101/c15_4_rc/src/main.rs
@@ -0,0 +1,21 @@
+#![allow(unused)]
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+fn main() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("after a: {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("after b: {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("after c: {}", Rc::strong_count(&a));
+    }
+    println!("c out of scope: {}", Rc::strong_count(&a));
+}
