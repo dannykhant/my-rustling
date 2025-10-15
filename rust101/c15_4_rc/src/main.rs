f72ba7f08003d2e1d1de114b71b581f6963b2cfe
dannykhant <dannypmkhant@gmail.com>
2025-10-15 23:16:30 +0700

    Add refcell example

--- rust101/c15_4_rc/src/main.rs
+++ rust101/c15_4_rc/src/main.rs
@@ -1,21 +1,27 @@
 #![allow(unused)]
-
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil
 }
 
 use crate::List::{Cons, Nil};
 use std::rc::Rc;
+use std::cell::RefCell;
 
 fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
     println!("after a: {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
     println!("after b: {}", Rc::strong_count(&a));
     {
-        let c = Cons(4, Rc::clone(&a));
+        let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
         println!("after c: {}", Rc::strong_count(&a));
     }
     println!("c out of scope: {}", Rc::strong_count(&a));
+
+    *value.borrow_mut() += 10;
+    println!("{b:?}");
 }
