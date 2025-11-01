4d6cb6f61a3810bb6f72ebbc6152e78a39c9d51f
dannykhant <dannypmkhant@gmail.com>
2025-11-01 22:25:27 +0700

    Add activity of testcase_list

---
+++ rustbyexample/r01_helloworld/src/testcase_list.rs
@@ -0,0 +1,21 @@
+// each write()! generates fmt::Result; ? operator for proper handling
+
+
+use std::fmt;
+
+pub struct List(pub Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        for (index, v) in vec.iter().enumerate() {
+            if index != 0 { write!(f, ", ")?; }
+            write!(f, "{}: {}", index, v)?;
+        }
+
+        write!(f, "]")
+    }
+}
