d557269b850ac1b408c855f393321375e44c94e0
dannykhant <dannypmkhant@gmail.com>
2025-11-14 21:04:13 +0700

    Add exsm: gigasecond

--- exercism/src/exercises.rs
+++ exercism/src/exercises.rs
@@ -2,3 +2,16 @@
 pub fn reverse(input: &str) -> String {
     input.chars().rev().collect()
 }
+
+
+// Returns a DateTime one billion seconds after start.
+use time::PrimitiveDateTime as DateTime;
+
+pub fn after(start: DateTime) -> DateTime {
+    
+    use time::Duration;
+
+    let end_odt = start.assume_utc() + Duration::seconds(1_000_000_000);
+    
+    DateTime::new(end_odt.date(), end_odt.time())
+}
--- exercism/src/main.rs
+++ exercism/src/main.rs
@@ -1,5 +1,7 @@
+#![allow(dead_code)]
 mod exercises;
 
 fn main() {
-    println!("{}", exercises::reverse("hello"));
+    use time::macros::datetime;
+    println!("{}", exercises::after(datetime!(2025-11-14 19:19:00)));
 }
