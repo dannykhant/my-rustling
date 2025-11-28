5a1c3a927fb053b3e1d1488a740ee4696fb91cae
dannykhant <dannypmkhant@gmail.com>
2025-11-28 21:25:44 +0700

    feat: [exsm] matching brackets

--- exercism/src/exercises.rs
+++ exercism/src/exercises.rs
@@ -265,3 +265,26 @@ impl<'a> HighScores<'a> {
         top
     }
 }
+
+
+// Determine if a string has balanced brackets.
+pub fn brackets_are_balanced(string: &str) -> bool {
+    let mut s = Vec::new();
+    
+    for c in string.chars() {
+        match c {
+            '(' | '[' | '{' => s.push(c),
+            ')' => {
+                if Some('(') != s.pop() { return false; }
+            }
+            ']' => {
+                if Some('[') != s.pop() { return false; }
+            }
+            '}' => {
+                if Some('{') != s.pop() { return false; }
+            }
+            _ => {}
+        }
+    }
+    s.is_empty()
+}
--- exercism/src/main.rs
+++ exercism/src/main.rs
@@ -2,6 +2,5 @@
 mod exercises;
 
 fn main() {
-    let hs = exercises::HighScores::new(&[30, 50, 20, 70, 40]);
-    println!("{:?}", hs.personal_top_three());
+    println!("{:?}", exercises::brackets_are_balanced("{[()}"));
 }
